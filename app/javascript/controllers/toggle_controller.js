8be8050629247a50da463467c674c42f8db9e72a
Laura Dumitriu <lauradumitriu01@gmail.com>
2024-09-16 22:46:35 -0400

    updated layout

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="toggle"
 export default class extends Controller {
-  static targets = ["togglableStack", "togglableProjects"]
+  static targets = ["togglableStack", "togglableProjects", "togglableContact"]
   connect() {
   }
   stack() {
@@ -10,5 +10,8 @@ export default class extends Controller {
   }
   projects() {
     this.togglableProjectsTarget.classList.toggle("d-none");
+  }
+  contact() {
+    this.togglableContactTarget.classList.toggle("d-none");
   }
 }
